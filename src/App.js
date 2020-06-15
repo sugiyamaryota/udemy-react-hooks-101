6973f5839a6073355324d449413a7ce3c088f933
杉山　良太 <sugiyama@anysense.co.jp>
2020-06-16 08:41:40 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const App = () => {
+  const [count,setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+  const reset = () => setCount(0)
+  const double = () => setCount(count * 2)
+
+  //const divide3 = () => setCount(count % 3 === 0 ?count / 3: count )
+  const divide3 = () => setCount(previousCount => {
+    if(previousCount % 3 === 0){
+      return previousCount / 3
+    }else{
+      return previousCount
+    }
+  } )
   return (
-    <div>This is a template</div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>+1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>+1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Rest</button>
+        <button onClick={double}>×２</button>
+        <button onClick={divide3}>3で割った商</button>
+      </div>
+    </>
   );
 }
 

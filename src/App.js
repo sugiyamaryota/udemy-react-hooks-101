55d840e42ea4a5ebf37458176075ddf1fcaeef10
杉山　良太 <sugiyama@anysense.co.jp>
2020-06-16 09:07:44 +0900

    multiple useState

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
 import React,{useState} from 'react';
 
-const App = () => {
-  const [count,setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+const App = props => {
+  // const initialStates = {
+  //   name: '',
+  //   price: 1000
+  // }
+  // const [name,setName] = useState(initialStates.name)
+  // const [price,setPrice] = useState(initialStates.price)
+  const [name,setName] = useState(props.name)
+  const [price,setPrice] = useState(props.price)
 
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-
-  const reset = () => setCount(0)
-  const double = () => setCount(count * 2)
-
-  //const divide3 = () => setCount(count % 3 === 0 ?count / 3: count )
-  const divide3 = () => setCount(previousCount => {
-    if(previousCount % 3 === 0){
-      return previousCount / 3
-    }else{
-      return previousCount
-    }
-  } )
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>+1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>+1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Rest</button>
-        <button onClick={double}>×２</button>
-        <button onClick={divide3}>3で割った商</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={()=> setPrice(price + 1)}>+1</button>
+      <button onClick={()=> setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;

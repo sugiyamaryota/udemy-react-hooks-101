472a163fa54fe272b7d4c8ab21528f535f759613
杉山　良太 <sugiyama@anysense.co.jp>
2020-06-18 22:27:49 +0900

    fix prop-brilling-issue

--- src/Event/index.js
+++ src/Event/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import AppContext from '../contexts/AppContext'
 import {DELETE_EVENT} from '../actions'
 
-const Event = ({event,dispatch}) => {
+const Event = ({event}) => {
+  const {dispatch} = useContext(AppContext)
   const handleClickDeleteButton = () => {
     const id = event.id
     const result = window.confirm(`イベント(id=${id})を本当に削除しますか？`)
--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,14 @@ import Events from './Events'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
 
-console.log(AppContext)
-
 const App = () => {
   const [state,dispatch] = useReducer(reducer,[])
 
   return (
-    <AppContext.Provider value={'hello world'}>
+    <AppContext.Provider value={{state,dispatch}}>
       <div className="container-fluid">
-        <EventFrom state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventFrom />
+        <Events />
       </div>
     </AppContext.Provider>
   );

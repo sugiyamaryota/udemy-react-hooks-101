14c5ff73a4d3dc317562d89b77070b3bbc907992
杉山　良太 <sugiyama@anysense.co.jp>
2020-06-18 23:02:00 +0900

    refact

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -43,7 +43,7 @@ const EventForm = () => {
           <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
         </div>
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-        <button className="btn btn-danger" onClick={delteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={delteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
       </form>
     </>
   )

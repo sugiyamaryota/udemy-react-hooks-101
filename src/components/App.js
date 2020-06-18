e9af9d50083036a5527c3fbe98c4dc94fdeb270f
杉山　良太 <sugiyama@anysense.co.jp>
2020-06-19 00:13:56 +0900

    localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useReducer} from 'react'
+import React,{useReducer,useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventFrom from './EventForm'
@@ -6,15 +6,19 @@ import Events from './Events'
 import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
-import operationLogs from '../reducers/operationLogs';
+
+const APP_KEY = 'appWithRedux'
 
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY)
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state,dispatch] = useReducer(reducer,initialState)
-
+  useEffect(()=>{
+    localStorage.setItem(APP_KEY,JSON.stringify(state))
+  },[state])
   return (
     <AppContext.Provider value={{state,dispatch}}>
       <div className="container-fluid">
